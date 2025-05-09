99c2403daa00d56fc57a9ee2ccac732f5948a835
Imran1720 <skimran1720@gmail.com>
2025-05-09 17:59:50 +0530

    added other services to the GameService

--- Assets/Scripts/Main/GameService.cs
+++ Assets/Scripts/Main/GameService.cs
@@ -1,18 +1,42 @@
+using ServiceLocator.Events;
+using ServiceLocator.Map;
 using ServiceLocator.Player;
+using ServiceLocator.Sound;
+using ServiceLocator.UI;
 using ServiceLocator.Utilities;
+using ServiceLocator.Wave;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class GameService : GenericMonoSingleton<GameService>
 {
-    public PlayerService playerService;
+    public PlayerService playerService { get; private set; }
+    public WaveService waveService { get; private set; }
+    public SoundService soundService { get; private set; }
+    public MapService mapService { get; private set; }
+    public EventService eventService { get; private set; }
 
-    [SerializeField] public PlayerScriptableObject playerScriptableObject;
+    [SerializeField] private UIService uIService;
+    public UIService UIService => uIService;
+
+    [SerializeField] private PlayerScriptableObject playerScriptableObject;
+
+    [SerializeField] private WaveScriptableObject waveScriptableObject;
+
+    [SerializeField] private MapScriptableObject mapScriptableObject;
+
+    [SerializeField] private SoundScriptableObject soundScriptableObject;
+    [SerializeField] private AudioSource audioEffects;
+    [SerializeField] private AudioSource backgroundMusic;
 
     private void Start()
     {
+        eventService = new EventService();
         playerService = new PlayerService(playerScriptableObject);
+        soundService = new SoundService(soundScriptableObject, audioEffects, backgroundMusic);
+        waveService = new WaveService(waveScriptableObject);
+        mapService = new MapService(mapScriptableObject);
     }
 
     private void Update()
--- Assets/Scripts/UI/UIService.cs
+++ Assets/Scripts/UI/UIService.cs
@@ -1,15 +1,15 @@
-using System.Collections.Generic;
-using UnityEngine;
-using TMPro;
-using UnityEngine.UI;
-using UnityEngine.SceneManagement;
-using ServiceLocator.Utilities;
 using ServiceLocator.Events;
+using ServiceLocator.Utilities;
 using ServiceLocator.Wave;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 namespace ServiceLocator.UI
 {
-    public class UIService : GenericMonoSingleton<UIService>
+    public class UIService : MonoBehaviour
     {
         [Header("Gameplay Panel")]
         [SerializeField] private GameObject gameplayPanel;
@@ -50,11 +50,11 @@ namespace ServiceLocator.UI
             nextWaveButton.onClick.AddListener(OnNextWaveButton);
             quitButton.onClick.AddListener(OnQuitButtonClicked);
             playAgainButton.onClick.AddListener(OnPlayAgainButtonClicked);
-            
+
             SubscribeToEvents();
         }
 
-        public void SubscribeToEvents() => EventService.Instance.OnMapSelected.AddListener(OnMapSelected);
+        public void SubscribeToEvents() => GameService.Instance.eventService.OnMapSelected.AddListener(OnMapSelected);
 
         public void OnMapSelected(int mapID)
         {
@@ -67,7 +67,7 @@ namespace ServiceLocator.UI
 
         private void OnNextWaveButton()
         {
-            WaveService.Instance.StarNextWave();
+            GameService.Instance.waveService.StarNextWave();
             SetNextWaveButton(false);
         }
 
