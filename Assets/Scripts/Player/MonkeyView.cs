4eac224a8002b6cc0dad809bb0980b547548d51d
Imran1720 <skimran1720@gmail.com>
2025-05-09 16:23:59 +0530

    Implemented Monkey Shoot Functionality

--- Assets/Scripts/Player/MonkeyView.cs
+++ Assets/Scripts/Player/MonkeyView.cs
@@ -1,5 +1,5 @@
-using UnityEngine;
 using ServiceLocator.Wave.Bloon;
+using UnityEngine;
 
 namespace ServiceLocator.Player
 {
@@ -24,6 +24,22 @@ namespace ServiceLocator.Player
 
             RangeSpriteRenderer.transform.localScale = new Vector3(radiusToSet, radiusToSet, 1);
             MakeRangeVisible(false);
+        }
+
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            if (collision.TryGetComponent<BloonView>(out BloonView bloonView))
+            {
+                controller.BloonEnteredRange(bloonView.Controller);
+            }
+        }
+
+        private void OnTriggerExit2D(Collider2D collision)
+        {
+            if (collision.TryGetComponent<BloonView>(out BloonView bloonView))
+            {
+                controller.BloonExitedRange(bloonView.Controller);
+            }
         }
 
         public void PlayAnimation(MonkeyAnimation animationToPlay) => monkeyAnimator.Play(animationToPlay.ToString(), 0);
